b83353e8680974245ea1e1ba7e81abf3f087b9ed
Illya Holubiev <121903042+HolubievIllya@users.noreply.github.com>
2023-07-17 14:28:37 +0300

    Update reverse.sh

--- Week-3/Lab-3/reverse.sh
+++ Week-3/Lab-3/reverse.sh
@@ -6,7 +6,7 @@ j=$#;   #get script input size
 while [[ $# -gt 1 ]]
 do
     rstring=$(echo $1 | rev);
-    echo "Reversing string $i-$1: $rstring";
+    echo "Reversing string $i:$1: $rstring";
     i=$((i + 1));   #increment the count
     shift 1;        #process the next argument
 done
